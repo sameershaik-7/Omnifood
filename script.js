a63e7b9a7168c53a2422c14e671ec1d7859ea1d8
Sameer Shaik <sameershaik@tutamail.com>
2026-06-11 23:00:24 +0530

    made the mobile navigation work

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date();
 yearEl.textContent = currentYear.getFullYear();
+
+// make mobile navigation work
+const navigationBtn = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+navigationBtn.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
