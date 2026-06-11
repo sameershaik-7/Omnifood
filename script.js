a556c54bb581baa3f9a7feb0279229b2544218d0
Sameer Shaik <sameershaik@tutamail.com>
2026-06-11 23:33:19 +0530

    Implemented smooth scrolling

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ const headerEl = document.querySelector(".header");
 navigationBtn.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
